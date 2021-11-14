4834df618af5f1f1ce44f3dd269b84b06a22aae2
Fawaz <fawazgames223@gmail.com>
2021-11-13 20:34:23 -0800

    Bassics are now complete and the publishing works on instagram! Now comes the actual poetry AI

--- app_dir/index.js
+++ app_dir/index.js
@@ -1,4 +1,4 @@
-const {API_TOKEN} = require('../app_dir/keys.js')
+const {API_TOKEN, FACEBOOK_TOKEN} = require('../app_dir/keys.js')
 const fs = require('fs')
 const axios = require('axios')
 const Buffer = require('buffer')
@@ -7,6 +7,8 @@ const Buffer = require('buffer')
 var GITHUB_API = "https://api.github.com/repos/fawaz-strawberry/"
 var ADD_FILE = "PoemTransformer/contents/images/"
 var FULL_PATH = "https://api.github.com/repos/fawaz-strawberry/PoemTran5sformer/contents/"
+var FACEBOOK_API = "https://graph.facebook.com/v12.0/"
+var FACEBOOK_API_SUBSET = "https://graph.facebook.com/"
 
 function getFileList(path){
 
@@ -55,6 +57,10 @@ function base64_encode(file) {
     return (bitmap).toString('base64');
 }
 
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+  }
+
 async function compareFiles(){
     
     local_files = await getFileList("../images/")
@@ -74,6 +80,72 @@ async function compareFiles(){
             console.log("Inserting " + local_files[i])
             encoding = base64_encode("../images/" + local_files[i])
             addImage(encoding, local_files[i])
+            await sleep(3000)
+        }
+    }
+
+    //Post an image to IG
+    postImage(github_files)
+}
+
+function postImage(files_to_add)
+{
+    var results = fs.readFileSync("posted_images.txt").toString()
+    
+    images = results.split("\r\n")
+    console.log(images)
+
+    for(var i = 0; i < files_to_add.length; i++){
+        if(images.indexOf(files_to_add[i]) === -1)
+        {
+            console.log("Posting image: " + files_to_add[i])
+            fs.writeFile(path="posted_images.txt", data=results + "\r\n" + files_to_add[i], () => {}) 
+
+            //Ask Github for the file URL of the image
+            console.log("Calling GITHUB API: " + GITHUB_API + ADD_FILE + files_to_add[i])
+            axios.get(GITHUB_API + ADD_FILE + files_to_add[i]).then(response => {
+                download_url = response["data"]["download_url"]
+                console.log("Download URL: " + download_url)
+
+                //Make the call to facebook accounts "me/accounts"
+                console.log("Calling Facebook API: " + FACEBOOK_API + "me/accounts?access_token=" + FACEBOOK_TOKEN)
+                axios.get(FACEBOOK_API + "me/accounts?access_token=" + FACEBOOK_TOKEN).then(response2 => {
+                    console.log(response2["data"])
+                    var page_id = response2["data"]["data"][0]["id"]
+                    console.log("Page ID: " + page_id)
+
+                    //Make the call to get facebook instagram accounts
+                    console.log("Calling Facebook API: " + FACEBOOK_API + page_id + "?fields=instagram_business_account&access_token=" + FACEBOOK_TOKEN)
+                    axios.get(FACEBOOK_API + page_id + "?fields=instagram_business_account&access_token=" + FACEBOOK_TOKEN).then(response3 => {
+                        var ig_id = response3["data"]["instagram_business_account"]["id"]
+                        console.log("IG ID: " + ig_id)
+
+                        //Create the Post and send it to the API servers
+                        console.log("Posting Image with: " + FACEBOOK_API + ig_id + "/media?image_url=" + download_url + "&access_token=" + FACEBOOK_TOKEN)
+                        axios.post(FACEBOOK_API + ig_id + "/media?image_url=" + download_url + "&access_token=" + FACEBOOK_TOKEN).then(response4 => {
+                            console.log(response4["data"])
+                            container_id = response4["data"]["id"]
+                            
+                            //Actually publish the image to Instagram
+                            console.log("Publishing Image with: " +FACEBOOK_API + ig_id + "/media_publish?creation_id=" + container_id + "&access_token=" + FACEBOOK_TOKEN)
+                            axios.post(FACEBOOK_API + ig_id + "/media_publish?creation_id=" + container_id + "&access_token=" + FACEBOOK_TOKEN).then(response5 => {
+                                console.log(response5["data"])
+                            })
+
+                        }).catch(err => {console.log("Failed POST Request: " + err)})
+
+                    }).catch(err => {console.log("Failed GET Request: " + err)})
+
+                }).catch(err => {console.log("Failed GET Request: " + err)})
+
+                //Make the call to get instagram business accounts
+                
+                //Make the call to media?image_url=insertURLHere&caption=insertCaptionHere
+
+                //Make the call to the API to send image
+
+            })
+            break
         }
     }
 }
@@ -91,5 +163,6 @@ function addImage(encoding, filename)
             console.log("Successful ?")
       })
 }
+
 
 compareFiles()
